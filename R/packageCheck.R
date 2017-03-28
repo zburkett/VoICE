5e79da500dcd3a154daa146b25eddd1cb041eb77
Zachary Burkett <zburkett@ucla.edu>
2017-03-27 17:27:37 -0700

    Updated packageCheck handling.

--- R/packageCheck.R
+++ R/packageCheck.R
@@ -1,3 +1,4 @@
+options(warn=-1)
 if(!file.exists('./R/.pkgSuccess'))
 {
 	chooseCRANmirror(ind=1)
@@ -10,11 +11,11 @@ if(!file.exists('./R/.pkgSuccess'))
 	
 	if(length(toInstall)>0)
 	{
+		source("https://bioconductor.org/biocLite.R",echo=F,verbose=F)
 		print('Installing missing R packages. Please wait. You should only have to do this once.')
 		for(pkg in toInstall)
 		{
 			#print(pkg)
-			source("https://bioconductor.org/biocLite.R")
 			if(.Platform$OS.type=="windows" & file.access(.libPaths()[length(.libPaths())], mode=2)==-1) #R library not writable in Windows, will make custom library and install packages there
 			{
                 if(!file.exists("./.libraries"))
@@ -22,24 +23,41 @@ if(!file.exists('./R/.pkgSuccess'))
                     system('mkdir ./.libraries')
                     system('attrib +h ".libraries"')
                 }
-				res = try(install.packages(pkg,lib="./.libraries"))
-				if(class(res)=="try-error")
-				{
-					res2 = biocLite(pkg,lib="./.libraries")
+				res = pkg %in% installed.packages(lib.loc="./.libraries")
 				
-					if (class(res2)=="try-error")
+				if(!res)
+				{
+					install.packages(pkg,lib="./.libraries")
+					res2 = pkg %in% installed.packages()
+					
+					if(!res2)
+					{
+						biocLite(pkg,lib="./.libraries")
+					}
+					
+					res3 = pkg %in% installed.packages(lib.loc="./.libraries")
+					
+					if(!res3)
 					{
 						errs = 1
 						stop(paste('Unable to install R package', pkg,". Please try to troubleshoot this yourself."))
 					}
 				}
 			}else{
-				res = try(install.packages(pkg))
-				if(class(res)=="try-error")
+				res = pkg %in% installed.packages()
+				if(!res)
 				{
-					res2 = biocLite(pkg)
-				
-					if (class(res2)=="try-error")
+					install.packages(pkg,verbose=F,quiet=T)
+					res2 = pkg %in% installed.packages()
+					
+					if(!res2)
+					{
+						biocLite(pkg,verbose=F,quiet=T)
+					}
+					
+					res3 = pkg %in% installed.packages()
+					
+					if(!res3)
 					{
 						errs = 1
 						stop(paste('Unable to install R package', pkg,". Please try to troubleshoot this yourself."))
@@ -55,10 +73,10 @@ if(!file.exists('./R/.pkgSuccess'))
 	if(!exists("errs"))
 	{
 		system(paste("touch","./R/.pkgSuccess"))
-		if(..Platform$OS.type=="windows")
+		if(.Platform$OS.type=="windows")
 		{
 			system(paste("attrib +h ./R/.pkgsuccess"))
 		}
 	}
 }
-
+options(warn=0)
